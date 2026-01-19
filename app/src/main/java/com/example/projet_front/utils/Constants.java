d447e419638772b3b36bdfb49ad88289e30dc9a8
imad-18 <imad.labrini@uit.ac.ma>
2026-01-19 21:49:26 +0100

    Update gitignore and modify Constants.java configuration

--- app/src/main/java/com/example/projet_front/utils/Constants.java
+++ app/src/main/java/com/example/projet_front/utils/Constants.java
@@ -1,6 +1,7 @@
 package com.example.projet_front.utils;
 
 public class Constants {
-    public static final String BASE_URL = "http://192.168.190.1:8080/";
-    //public static final String BASE_URL = "http://10.0.2.2:8080/";
+    //public static final String BASE_URL = "http://192.168.190.1:8080/";
+    public static final String BASE_URL = "http://10.0.2.2:8080/";
+    //public static final String BASE_URL = "http://10.244.67.6:8080/";
 }
